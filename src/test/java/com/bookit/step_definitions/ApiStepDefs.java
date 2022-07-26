be9f8cbf699b305e0428b5786829ab2b415fe8a4
Laman <laman_garayeva@outlook.com>
2022-07-26 12:04:02 +0200

    verify info about logged user from api and db

--- src/test/java/com/bookit/step_definitions/ApiStepDefs.java
+++ src/test/java/com/bookit/step_definitions/ApiStepDefs.java
@@ -2,12 +2,17 @@ package com.bookit.step_definitions;
 
 import com.bookit.utilities.BookItApiUtil;
 import com.bookit.utilities.ConfigurationReader;
+import com.bookit.utilities.DBUtils;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.junit.Assert;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 import static io.restassured.RestAssured.given;
 
@@ -15,12 +20,13 @@ public class ApiStepDefs {
 
     String token;
     Response response;
+    String emailGlobal;
 
     @Given("I logged Bookit api using {string} and {string}")
     public void i_logged_Bookit_api_using_and(String email, String password) {
 
         token = BookItApiUtil.generateToken(email, password);
-
+        emailGlobal = email;
     }
 
     @When("I get the current user information from api")
@@ -43,5 +49,31 @@ public class ApiStepDefs {
 
     }
 
+    @Then("the information about current user from api and database should match")
+    public void the_information_about_current_user_from_api_and_database_should_match() {
+
+        // get information from database
+        String query = "select firstname, lastname, role from users\n" +
+                "where email = '"+emailGlobal+"'";
+
+        Map<String, Object> dbMap = DBUtils.getRowMap(query);
+        System.out.println("dbMap = " + dbMap);
+
+        String expectedFirstName = (String) dbMap.get("firstname");
+        String expectedLastName = (String) dbMap.get("lastname");
+        String expectedRole = (String) dbMap.get("role");
+
+        // get information from api
+        JsonPath jsonPath = response.jsonPath();
+        String actualFirstName = jsonPath.getString("firstName");
+        String actualLastName = jsonPath.getString("lastName");
+        String actualRole = jsonPath.getString("role");
+
+        // compare database and api
+        Assert.assertEquals(expectedFirstName, actualFirstName);
+        Assert.assertEquals(expectedLastName, actualLastName);
+        Assert.assertEquals(expectedRole, actualRole);
+
+    }
 
 }
--- src/test/resources/features/user.feature
+++ src/test/resources/features/user.feature
@@ -1,10 +1,15 @@
 @smoke
 Feature: User Verification
 
-  @wip
   Scenario: verify information about logged user
     Given I logged Bookit api using "wcanadinea@ihg.com" and "waverleycanadine"
     When I get the current user information from api
     Then status code should be 200
+
+  @db @wip
+  Scenario: verify information about logged user from api and database
+    Given I logged Bookit api using "wcanadinea@ihg.com" and "waverleycanadine"
+    When I get the current user information from api
+    Then the information about current user from api and database should match
 
 
