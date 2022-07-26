d24e0ffea8add849172f52f2cef601f476ad4f5b
Laman <laman_garayeva@outlook.com>
2022-07-26 10:28:45 +0200

    step definitions

---
+++ src/test/java/com/bookit/step_definitions/ApiStepDefs.java
@@ -0,0 +1,46 @@
+package com.bookit.step_definitions;
+
+import com.bookit.utilities.BookItApiUtil;
+import com.bookit.utilities.ConfigurationReader;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.junit.Assert;
+
+import static io.restassured.RestAssured.given;
+
+public class ApiStepDefs {
+
+    String token;
+    Response response;
+
+    @Given("I logged Bookit api using {string} and {string}")
+    public void i_logged_Bookit_api_using_and(String email, String password) {
+
+        token = BookItApiUtil.generateToken(email, password);
+
+    }
+
+    @When("I get the current user information from api")
+    public void i_get_the_current_user_information_from_api() {
+        System.out.println("token = " + token);
+
+        // send GET request "/api/users/me" endpoint to get current user info
+        response = given().accept(ContentType.JSON)
+                .and()
+                .header("Authorization", token)
+                .when().get(ConfigurationReader.get("base_url") + "/api/users/me");
+
+    }
+
+    @Then("status code should be {int}")
+    public void status_code_should_be(int statusCode) {
+
+        Assert.assertEquals(statusCode, response.statusCode());
+
+    }
+
+
+}
