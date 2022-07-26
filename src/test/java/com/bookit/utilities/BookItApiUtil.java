b0d12de97912df086c94db61159146b978876591
Laman <laman_garayeva@outlook.com>
2022-07-26 10:29:26 +0200

    utility method for generating token

---
+++ src/test/java/com/bookit/utilities/BookItApiUtil.java
@@ -0,0 +1,21 @@
+package com.bookit.utilities;
+
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+
+import static io.restassured.RestAssured.given;
+
+public class BookItApiUtil {
+
+    public static String generateToken(String email, String password){
+
+        Response response = given().accept(ContentType.JSON)
+                .queryParams("email", email, "password", password)
+                .when().get(ConfigurationReader.get("base_url") + "/sign");
+
+        String token = response.path("accessToken");
+
+        return "Bearer "+token;
+    }
+
+}
