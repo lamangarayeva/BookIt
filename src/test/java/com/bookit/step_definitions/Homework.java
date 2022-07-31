3425a72e5a75448ef73c7bad73470002af689bcc
Laman <laman_garayeva@outlook.com>
2022-07-31 12:07:16 +0200

    homework

---
+++ src/test/java/com/bookit/step_definitions/Homework.java
@@ -0,0 +1,17 @@
+package com.bookit.step_definitions;
+
+public class Homework {
+
+    // try to get name,role,batch number, campus, team name from api for one student
+    // it will be multiple api request
+    //responses returns batch name team name with students information
+    //first make sure your student is inside the responses then get this info
+
+    // prepare one list of information about student and compare with ui
+
+    //UI vs API
+
+
+    //verify same information vs
+    //connect db and get the same information which requires joins multiple tables
+}
